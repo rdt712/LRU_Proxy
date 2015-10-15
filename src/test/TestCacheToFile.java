e461fb74204764e20cd54bd7b473d4660994b935
Ryan Toler <tolerr1@nku.edu>
2015-10-15 11:02:45 -0400

    Added TestCacheToFile.java

---
+++ src/test/TestCacheToFile.java
@@ -0,0 +1,15 @@
+package test;
+
+import main.*;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TestCacheToFile {
+	
+	private String directory = "";
+	
+	@Test
+	public void removeTest() {
+		CacheToFile ctf = new CacheToFile(directory);
+	}
+}
