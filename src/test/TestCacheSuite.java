32499105d50966aaf59d9ab0a5067afa75e5a47b
Brian <Brian@10.206.3.238>
2015-10-19 17:33:59 -0400

    Changes to TestCacheSuite

--- src/test/TestCacheSuite.java
+++ src/test/TestCacheSuite.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(value = org.junit.runners.Suite.class)
 @SuiteClasses(value = {TestCacheLog.class, TestCacheRequest.class,
-						TestCacheToFile.class, })
+						TestCacheToFile.class, TestCacheList.class})
 public class TestCacheSuite {
 	public static void main(String[] args) {
 		org.junit.runner.JUnitCore.runClasses();
