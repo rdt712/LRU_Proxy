9c1eb0f7487ce8e99b2415745e0282b8f1da7403
Joey Drees <dreesj1@nku.edu>
2015-11-12 11:50:12 -0500

    added new requestLog method in CacheLog.java

--- src/main/CacheLog.java
+++ src/main/CacheLog.java
@@ -1,6 +1,9 @@
 package main;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -18,13 +21,16 @@ public class CacheLog
 {
 
 	private BufferedWriter out;
+	private BufferedReader in;
 	private String filename;
 	private SimpleDateFormat format;
+	private String req_log;
 	
 	private String returnString;
 	
 	public CacheLog(String directory)
 	{
+		req_log = directory + "/server_req.log";
 		filename=directory+"/output.log";
 		returnString="\n";
 		format =
@@ -87,6 +93,26 @@ public class CacheLog
 		}
 		catch (Exception e)
 		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void requestLog(Socket client, int req_num)
+	{
+		try {
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			out = new BufferedWriter(new FileWriter(req_log, true));
+			out.write(req_num);
+			String input = in.readLine();
+			if (input != null)
+				out.write(input);
+			Calendar cal = Calendar.getInstance();
+			String dateString=format.format(cal.getTime());
+			out.write(dateString + "\n");
+			in.close();
+			out.close();
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
